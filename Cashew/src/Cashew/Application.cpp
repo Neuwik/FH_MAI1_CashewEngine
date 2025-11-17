c408933d37946d9242fb646ffa1dd8073a00d31b
Dominik Neuwirth <domineu04@gmail.com>
2025-11-17 17:02:03 +0100

    Window Events

--- Cashew/src/Cashew/Application.cpp
+++ Cashew/src/Cashew/Application.cpp
@@ -1,13 +1,15 @@
 #include "precompileheaders.h"
 #include "Application.h"
-#include "Cashew/Events/ApplicationEvent.h"
 #include "Log.h"
 
 namespace Cashew
 {
+#define BIND_EVENT_FN(x) std::bind(&Application::x, this, std::placeholders::_1)
+
 	Application::Application()
 	{
 		m_Window = std::unique_ptr<Window>(Window::Create());
+		m_Window->SetEventCallback(BIND_EVENT_FN(OnEvent));
 	}
 
 	Application::~Application()
@@ -21,5 +23,19 @@ namespace Cashew
 		{
 			m_Window->OnUpdate();
 		}
+	}
+
+	void Application::OnEvent(Event& e)
+	{
+		EventDispatcher dispatcher(e);
+		dispatcher.Dispatch<WindowCloseEvent>(BIND_EVENT_FN(OnWindowClose));
+
+		CASHEW_CORE_TRACE("{0}", e);
+	}
+
+	bool Application::OnWindowClose(WindowCloseEvent& e)
+	{
+		m_Running = false;
+		return true;
 	}
 }
